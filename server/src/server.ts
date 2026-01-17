b6ead63a28d2469e0ce1efe4ab1e8f3716592c12
jitesh <jitesh.kumar05official@gmail.com>
2026-01-17 22:59:23 +0530

    adding docs and deployement necessary links

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,7 +10,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:3001", 
+    "https://email-scheduler-red.vercel.app/" 
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 app.get('/health', (_req, res) => {
